49d2dcc1796cad7a2aa3e3b1dc35830499a8af01
mde <mde@yammer-inc.com>
2012-08-08 10:39:17 -0700

    Get comparison values properly escaped and quoted

--- test/query.js
+++ test/query.js
@@ -1,13 +1,16 @@
 var model = require('../lib/model')
   , Query = require('../lib/mapper/query').Query
+  , operation = require('../lib/mapper/operation')
+  , comparison = require('../lib/mapper/comparison')
   , utils = require('utilities')
   , assert = require('assert')
   , User = require('./fixtures/user')
   , tests;
 
 var tests = {
-  'test parse': function () {
-    var query = new Query(User, {login: 'foo', firstName: null}, {});
+  'test condition': function () {
+    var query = new Query(User, {login: {like: 'foo'}, firstName: null}, {});
+    assert.ok(query.conditions instanceof operation.AndOperation);
     console.log(query.conditions.toString());
   }
 
