8fbdb1f654f5ec715d245a137357e7eb72ab468a
mde <mde@yammer-inc.com>
2012-08-13 00:20:53 -0700

    Case-insensitive LIKE and more tests

--- test/adapters/sql/postgres.js
+++ test/adapters/sql/postgres.js
@@ -1,14 +1,16 @@
 
-var Adapter = require('../../../lib/adapters/sql/postgres').Adapter
+var utils = require('utilities')
+  , Adapter = require('../../../lib/adapters/sql/postgres').Adapter
   , Query = require('../../../lib/query/query').Query
-  , Zooby = require('../../fixtures/zooby').Zooby
   , model = require('../../../lib')
   , generator = require('../../../lib/generators/sql')
   , adapter
   , assert = require('assert')
   , currentId
   , tests
-  , _runOnce;
+  , testItems = []
+  , _runOnce
+  , Zooby = require('../../fixtures/zooby').Zooby;
 
 tests = {
   'before': function (next) {
@@ -120,6 +122,69 @@ tests = {
         assert.equal(data.length, 0);
         next();
       });
+    });
+  }
+
+, 'test save collection': function (next) {
+    var dt = new Date();
+    testItems.push(Zooby.create({
+      foo: 'FOO'
+    , zong: utils.date.add(dt, 'day', -1)
+    }));
+    testItems.push(Zooby.create({
+      foo: 'BAR'
+    , zong: utils.date.add(dt, 'day', -2)
+    }));
+    testItems.push(Zooby.create({
+      foo: 'BAZ'
+    , zong: utils.date.add(dt, 'day', -3)
+    }));
+    Zooby.save(testItems, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      next();
+    });
+  }
+
+, 'test all by string equality': function (next) {
+    Zooby.all({foo: 'FOO'}, {}, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      assert.equal(data.length, 1);
+      assert.equal(testItems[0].foo, data[0].foo);
+      next();
+    });
+  }
+
+, 'test all by string LIKE case-sensitive': function (next) {
+    Zooby.all({foo: {'like': 'B'}}, {}, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      assert.equal(data.length, 2);
+      next();
+    });
+  }
+
+, 'test all by string LIKE case-insensitive bool': function (next) {
+    Zooby.all({foo: {'like': 'b'}}, {lowercase: true}, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      assert.equal(data.length, 2);
+      next();
+    });
+  }
+
+, 'test all by LIKE case-insensitive array': function (next) {
+    Zooby.all({foo: {'like': 'b'}}, {lowercase: ['foo']}, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      assert.equal(data.length, 2);
+      next();
     });
   }
 
