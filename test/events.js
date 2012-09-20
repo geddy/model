75a5b3ee4dd8637dd2f63ace0caf8a6a6590944a
mde <mde@yammer-inc.com>
2012-09-19 18:10:26 -0700

    Fixed static events, added tests

--- test/events.js
+++ test/events.js
@@ -45,26 +45,6 @@ tests = {
     var user = User.create(_params);
   }
 
-, 'emit static beforeUpdate': function (next) {
-    User.once('beforeUpdate', function (u) {
-      assert.ok(u instanceof User);
-      assert.equal('zzz', u.login);
-      next();
-    });
-    var user = User.create(_params);
-    user.updateProperties({login: 'yyz'});
-  }
-
-, 'emit static update': function (next) {
-    User.once('update', function (u) {
-      assert.ok(u instanceof User);
-      assert.equal('yyz', u.login);
-      next();
-    });
-    var user = User.create(_params);
-    user.updateProperties({login: 'yyz'});
-  }
-
 , 'emit static beforeValidate': function (next) {
     User.once('beforeValidate', function (u, p) {
       assert.ok(u instanceof User);
@@ -99,6 +79,50 @@ tests = {
     });
     var user = User.create(_params);
     user.save(function () {});
+  }
+
+, 'emit static beforeUpdate': function (next) {
+    User.once('beforeUpdate', function (u) {
+      assert.ok(u instanceof User);
+      next();
+    });
+    var user = User.create(_params);
+    user.save(function () {
+      user.save(function () {});
+    });
+  }
+
+, 'emit static update': function (next) {
+    User.once('update', function (res) {
+      assert.ok(res);
+      next();
+    });
+    var user = User.create(_params);
+    user.save(function () {
+      user.save(function () {});
+    });
+  }
+
+, 'emit static beforeRemove': function (next) {
+    User.once('beforeRemove', function (res) {
+      assert.ok(res);
+      next();
+    });
+    var user = User.create(_params);
+    user.save(function () {
+      User.remove(user.id, function () {});
+    });
+  }
+
+, 'emit static remove': function (next) {
+    User.once('remove', function (res) {
+      assert.ok(res);
+      next();
+    });
+    var user = User.create(_params);
+    user.save(function () {
+      User.remove(user.id, function () {});
+    });
   }
 
 , 'last': function () {}
