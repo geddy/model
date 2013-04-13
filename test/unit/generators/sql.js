ad2dd5fde1debedbde539a4250c054aff0770202
mde <mde@yammer-inc.com>
2013-04-12 19:08:19 -0700

    More unit tests for SQL generators

--- test/unit/generators/sql.js
+++ test/unit/generators/sql.js
@@ -8,13 +8,45 @@ var assert = require('assert')
 
 arrIncl = function (array, item) {
   return array.some(function (arrItem) {
-    var str = utils.string.trim(arrItem.toLowerCase());
-    return str.indexOf(item) == 0;
+    return strIncl(arrItem, item);
   });
 };
 
+strIncl = function (searchIn, searchFor) {
+    var sIn = utils.string.trim(searchIn.toLowerCase());
+    var sFor = utils.string.trim(searchFor.toLowerCase());
+    return sIn.indexOf(sFor) == 0;
+};
+
 tests = {
-  'test createTable SQL': function () {
+
+  'columnStatement': function () {
+    var sql = generator.columnStatement({
+      name: 'barBazQux'
+    , datatype: 'string'
+    });
+    assert.ok(strIncl(sql, 'bar_baz_qux varchar(256)'));
+  }
+
+, 'createTableStatement': function () {
+    var sql = generator.createTableStatement('Zerb', {
+      foo: {
+        name: 'foo'
+      , datatype: 'string'
+      }
+    });
+    sql = sql.split('\n');
+    assert.ok(arrIncl(sql,
+        'drop table if exists zerbs;'));
+    assert.ok(arrIncl(sql,
+        'create table zerbs ('));
+    assert.ok(arrIncl(sql,
+        'id varchar(256) primary key'));
+    assert.ok(arrIncl(sql,
+        'foo varchar(256)'));
+  }
+
+, 'createTable with single model object': function () {
     var sql = generator.createTable(['Zooby']);
     sql = sql.split('\n');
     assert.ok(arrIncl(sql,
