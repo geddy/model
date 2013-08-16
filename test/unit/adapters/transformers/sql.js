c6e0127a13aaf74ff031b46b1df222ff835e9304
mde <mde@fleegix.org>
2013-08-16 14:10:28 -0700

    Stub out unit tests for transformers

---
+++ test/unit/adapters/transformers/sql.js
@@ -0,0 +1,32 @@
+var utils = require('utilities')
+  , assert = require('assert')
+  , tests
+  , tfr = require('../../../../lib/adapters/transformers/sql')
+  , converter = require('../../../../lib/adapters/sql/converter');
+
+utils.mixin(tfr, converter);
+
+tests = {
+  'transform comparison field name': function () {
+    var name = tfr.transformComparisonFieldName({
+          model: 'User'
+        , field: 'foo'
+        , opts: {}
+        , datatype: 'string'
+        });
+    assert.equal('users."foo"', name);
+  }
+
+, 'transform comparison field name, nocase': function () {
+    var name = tfr.transformComparisonFieldName({
+          model: 'User'
+        , field: 'foo'
+        , opts: {nocase: true}
+        , datatype: 'string'
+        });
+    assert.equal('LOWER(users."foo")', name);
+  }
+
+};
+
+module.exports = tests;
