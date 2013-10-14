a1fd5f035cb6b5f1fa2b7095fa29134e60a56a67
mde <mde@fleegix.org>
2013-10-13 19:47:32 -0700

    Filesystem adaper, to be new default

---
+++ test/integration/adapters/filesystem/index.js
@@ -0,0 +1,54 @@
+var utils = require('utilities')
+  , assert = require('assert')
+  , model = require('../../../../lib')
+  , helpers = require('.././helpers')
+  , Adapter = require('../../../../lib/adapters/filesystem').Adapter
+  , adapter
+  , tests
+  , shared = require('../shared');
+
+tests = {
+  'before': function (next) {
+    var relations = helpers.fixtures.slice()
+      , models = [];
+    adapter = new Adapter();
+
+    model.adapters = {};
+    relations.forEach(function (r) {
+      model[r].adapter = adapter;
+      models.push({
+        ctorName: r
+      });
+    });
+
+    model.registerDefinitions(models);
+
+    adapter.createTable(Object.keys(model.adapters), next);
+
+    // FIXME: registration should do this, no?
+    model.adapters.filesystem = adapter;
+  }
+
+, 'after': function (next) {
+    model.adapters.filesystem.disconnect(next);
+  }
+
+, 'test create adapter': function () {
+    assert.ok(adapter instanceof Adapter);
+  }
+
+
+};
+
+for (var p in shared) {
+  if (p == 'beforeEach' || p == 'afterEach') {
+    tests[p] = shared[p];
+  }
+  else {
+    tests[p + ' (Filesystem)'] = shared[p];
+  }
+}
+
+module.exports = tests;
+
+
